0441e2451a004c183a9e17544ccbff8c38f2bd7d
Yannes Meneguelli <yannes_meneguelli@hotmail.com>
2020-03-24 14:06:59 -0300

    Initial Commit

---
+++ Project 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 1
+//
+//  Created by Yannes Meneguelli on 24/03/20.
+//  Copyright © 2020 Yannes Meneguelli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
